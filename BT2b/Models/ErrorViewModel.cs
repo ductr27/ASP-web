10470836ee281ce9b589b5b8a3dca7c1bb884d38
Vu Trung Duc <ductrung40272@gmail.com>
2024-08-28 23:37:44 +0700

    BT2b (BTVN)
    Da hoan thanh

---
+++ BT2b/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BT2b__BTVN_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
