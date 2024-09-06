3063224b2c1b03bc520741a65abbae93d8f0572e
Duc Trung <ductrung40272@gmail.com>
2024-09-07 04:45:36 +0700

    BT5

---
+++ BT5/Controllers/TheLoaiController.cs
@@ -0,0 +1,27 @@
+﻿using BiaTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BT5.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 9";
+            ViewBag.Thang = "Tháng 11";
+            ViewData["Nam"] = "Năm 2001";
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Kiếm hiệp"
+            };
+
+            return View(theloai);
+        }
+    }
+}
---
+++ BT5/Models/TheLoaiViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BiaTap05.Models
+{
+    public class TheLoaiViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
