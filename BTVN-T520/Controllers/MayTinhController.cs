a64dd4a255bba05fd26d747df544eee482adf07c
Duc Trung <ductrung40272@gmail.com>
2024-09-11 04:28:47 +0700

    BTVN-T520

---
+++ BTVN-T520/Controllers/MayTinhController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTVN_TH20.Controllers
+{
+    public class MayTinhController : Controller
+    {
+        public IActionResult MayTinh(double a, double b, string pheptinh)
+        {
+            double ketqua = 0;
+            string thongbao = "";
+            if (string.IsNullOrWhiteSpace(pheptinh))
+            {
+                ViewBag.KetQua = "Phép tính không hợp lệ!";
+                return View();
+            }
+            switch (pheptinh.ToLower())
+            {
+                case "cong":
+                    ketqua = a + b;
+                    break;
+                case "tru":
+                    ketqua = a - b;
+                    break;
+                case "nhan":
+                    ketqua = a * b;
+                    break;
+                case "chia":
+                    if (b != 0)
+                    {
+                        ketqua = a / b;
+                    }
+                    else
+                    {
+                        thongbao = "Không thể chia cho 0";
+                    }
+                    break;
+                default:
+                    thongbao = "phép tính không hợp lệ!";
+                    break;
+            }
+            if (string.IsNullOrEmpty(thongbao))
+            {
+                ViewBag.KetQua = $"Kết qua của phép {pheptinh} giữa {a} và {b} la: {ketqua}";
+            }
+            else
+            {
+                ViewBag.KetQua = thongbao;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ BTVN-T520/Controllers/Tuan02controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTVN_TH20.Controllers
+{
+    public class Tuan02controller : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            
+            {
+                ViewBag.HoTen = "Vu Trung Duc";
+                ViewBag.MSSV = "1822041161";
+                ViewData["Nam"] = "Năm 2077";
+                return View();
+            }
+
+
+        }
+    }
+}
